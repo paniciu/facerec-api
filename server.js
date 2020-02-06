269247d765a2da873b3df97f9d1028bf7c019438
Sylaratty <apaniciu@gmail.com>
2020-02-06 19:25:02 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const database = {
+    users: [
+        {
+            id: '1',
+            name: 'John Doe',
+            email: 'john@gmail.com',
+            password: 'john123',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '2',
+            name: 'Jane Doe',
+            email: 'jane@gmail.com',
+            password: 'jane123',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '3',
+            name: 'Attila',
+            email: 'attila@gmail.com',
+            password: 'atti123',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send(database.users);
+})
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+            res.json(database.users[0]);
+        } else {
+            res.status(400).json('error logging in');
+        }
+})
+
+app.post('/register', (req, res) => {
+    const { email, name, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: name,
+        email: email,
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length-1]);
+})
+
+app.get('/profile/:id', (req,res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+})
+
+app.put('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++;
+            return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+})
+
+app.listen(3000, () => {
+    console.log('app is running on port 3000');
+})
